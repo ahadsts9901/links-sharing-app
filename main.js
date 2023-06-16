8f30dc90a15c10c0660c2cfde5cac8b1f1f07d06
Muhammad Ahad <120649081+ahadsts9901@users.noreply.github.com>
2023-06-16 18:55:35 +0500

    firebase data created

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDDkH9pqIcdWmRlXHdyC2G0z0Lhlej5dCw",
+    authDomain: "links-sharing-b9c2a.firebaseapp.com",
+    projectId: "links-sharing-b9c2a",
+    storageBucket: "links-sharing-b9c2a.appspot.com",
+    messagingSenderId: "606062782904",
+    appId: "1:606062782904:web:c4a1a96a5f42be8236c74d",
+    // measurementId: "G-NRHCLX7W6D"
+}
+
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+
 let section = document.querySelector(".section")
 
-function addLink(event){
+function addLink(event) {
     event.preventDefault()
-
     let input = document.querySelector('.input').value
+
+    db.collection("users").add({
+        link: 'https://youtube.com'
+    })
+    .then((docRef) => {
+        renderLink()
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+    });
 
     let row = document.createElement("div")
     row.className += " rowScroll"
@@ -11,8 +33,8 @@ function addLink(event){
 
     let link = document.createElement('a')
     link.className += " scrollX"
-    link.setAttribute("target","_blank")
-    link.setAttribute("href",`${input}`)
+    link.setAttribute("target", "_blank")
+    link.setAttribute("href", `${input}`)
     link.innerText = input
     row.appendChild(link)
 
@@ -23,4 +45,5 @@ function addLink(event){
 
 
     document.querySelector('.input').value = ""
+    
 }
